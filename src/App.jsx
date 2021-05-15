8401ed57e3431a7dba1431041caacb96164424a3
nothacker <nothacker@gmail.com>
2021-05-15 11:11:55 +0400

    create forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import './css/App.css';
-import React, { Component } from 'react'
-import Users from './components/Users';
+import React, { Component  } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect, Link, NavLink } from 'react-router-dom'; 
+import Index from './pages/Index';
+import About from './pages/About';
+import Forms from './pages/Forms';
 
 export default class App extends Component {
    
 
   render() {
     return (
-    <div className="App">
-      <Users />
-    </div>
+      <div className="App">
+        <Router>
+          <nav>
+            <ul>
+              <li><NavLink activeClassName="shlor" exact to="/">Home</NavLink></li>
+              <li><NavLink activeClassName="shlor" exact to="/about">  About </NavLink> </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Index} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/forms" component={Forms} />
+
+            <Redirect from="/index" to="/" />
+          </Switch>
+        </Router>
+      </div>
     )
   }
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About  extends Component {
+    render() {
+        return (
+            <div>
+                Hello, this is not index, this is about page
+            </div>
+        )
+    }
+}
